1f70119fd43b1a37b40c9bdde5d12805ea0af59e
mandagouveia <amanda_gouveia_@hotmail.com>
2021-08-11 22:29:26 -0300

    Criei teste para incluir um pet

--- ProjetoFinalAccenture/qualityteam/PetStore/src/test/java/petstore/Pet.java
+++ ProjetoFinalAccenture/qualityteam/PetStore/src/test/java/petstore/Pet.java
@@ -1,5 +1,49 @@
+// 1- Pacote
 package petstore;
 
-public class Pet {
 
+// 2- Bibliotecas
+
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+// 3- Classe
+public class Pet {
+        //3.1 - Atributos
+        String uri = "https://petstore.swagger.io/v2/pet";
+
+
+        //3.2 - Métodos e Funções
+        public String lerJson(String caminhoJson) throws IOException {
+
+                return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+
+        }
+        // Incluir - Create - Post
+        @Test
+        public void incluirPet() throws IOException {
+                String jsonBody = lerJson("db/pet1.json");
+
+                // Sintaxe Gherkin
+                //Dado - Quando - Então
+
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .body(jsonBody)
+
+                .when()
+                        .post(uri)
+
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                ;
+        }
 }
